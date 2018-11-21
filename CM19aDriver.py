9fe206b69662e2a3f2eafd87b739aaef58034f22
Greg Reive <ubeaut58@gmail.com>
2018-11-21 16:45:42 +1100

    finished and all working...yay

--- CM19aDriver.py
+++ CM19aDriver.py
@@ -848,11 +848,11 @@ def startMQTT( client, host, port ):
                     house = b.group(1).upper()
                     unit = b.group(2).upper()
                     command = b.group(3).lower()
-                    command.capitalize()
-                    print "Command button press: %s" % command
-                    print "Unit button press: %s" % unit
-                    print "House button press: %s" % house
-                    if command in ['on', 'off', 'Brightbuttonpressed', 'Dimbuttonpressed']:
+                    command = command.capitalize()
+                    #print "Command button press: %s" % command
+                    #print "Unit button press: %s" % unit
+                    #print "House button press: %s" % house
+                    if command in ['On', 'Off', 'Brightbuttonpressed', 'Dimbuttonpressed']:
                         client.publish( "cmnd/%s%s/Power" % (house, unit), command )
                         log.info( "cmnd/%s%s/Power %s" % ( house, unit, command ) )
                     else:
